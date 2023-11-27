e6f02f2cf393efef03b141925cb85fcb33b818e6
dev-pritam-2005 <150615538+dev-pritam-2005@users.noreply.github.com>
2023-11-27 02:39:29 +0000

    program re build

--- 01_basics/01_variables.js
+++ 01_basics/01_variables.js
@@ -2,3 +2,9 @@ const accountId = 433625
 let accountEmail =" pritamdutta@gmail.com"
 var accountPassward = "25469"
 accountCity = "west bengal"
+
+accountPassward="21212121"
+accountCity ="bangaluru"
+console.log(accountId)
+
+console.table([accountId,accountEmail,accountPassward,accountCity])
