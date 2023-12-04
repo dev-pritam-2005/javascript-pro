ad52fa89f714f5a30a21684798a75d96ab4d9d20
dev.pritam <150615538+dev-pritam-2005@users.noreply.github.com>
2023-12-04 18:53:55 +0000

    new date 5th december

---
+++ 01_basics/03_conversitionsOperations.js
@@ -0,0 +1,24 @@
+let score = "55abc"
+
+// console.log(typeof score);
+// console.log(typeof(score));
+
+
+let valueInNumber = Number(score);
+// console.log(typeof(valueInNumber));
+// console.log(valueInNumber);  //output is NaN = not a Number 
+
+
+// let isLoggedIn = 1  output true
+// let isLoggedIn = ""  output false
+let isLoggedIn = "pritam" // out put  true
+let booleanIsLoggedIn = Boolean(isLoggedIn)
+// console.log(booleanIsLoggedIn);
+
+
+
+let someNumber = 56
+let stringNumber = String(someNumber)
+
+console.log(stringNumber);
+console.log(typeof stringNumber );
