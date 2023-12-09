10cf622f5529f8cbf2f27354e221308d72cbba8a
dev.pritam <150615538+dev-pritam-2005@users.noreply.github.com>
2023-12-09 10:39:38 +0000

    new to strings

---
+++ 01_basics/05_string.js
@@ -0,0 +1,19 @@
+const name = "pritamdutta"
+const repoCount = 50
+ 
+// console.log(name + repoCount);
+
+
+console.log(`hello my name is ${name} and my repo count is ${repoCount}`);
+
+const gameName = new String('pritamDutta.DEV')
+
+console.log(gameName[0]);
+console.log(gameName.length);
+console.log(gameName.toUpperCase());
+
+console.log(gameName.charAt(5));
+const newString = gameName.substring(0,4)
+console.log(newString);
+
+
